5690fa18a39a921a6fb8a529638e0f67344a4904
lclass <88320133+lclass@users.noreply.github.com>
2022-04-30 22:14:08 +0200

    idk some update

--- cutting_csv.py
+++ cutting_csv.py
@@ -17,17 +17,17 @@ class CuttingData:
         length_total = len(total_num)
         search_id_list = self.df['srch_id'].to_list()
 
-        for i in range(round(int(length_total / 20))):
+        for i in range(round(int(length_total / 40))):
             column_id = random.choice(search_id_list)
             all_rows = self.df[self.df['srch_id'].values == column_id]
             self.shortened_df = pd.concat([self.shortened_df, all_rows], ignore_index=True)
             if i % 100 == 0:
-                print(f"""{i} of {round((length_total / 20))} search ids appended to the dataframe
+                print(f"""{i} of {round((length_total / 40))} search ids appended to the dataframe
 -----------------------------------------------------------------------------------------------------------------------
 """)
 
     def saving_csv(self):
-        self.shortened_df.to_csv(r'data/shortened_data/data_10000.csv', index=False, header=True)
+        self.shortened_df.to_csv(r'data/shortened_data_5000.csv', index=False, header=True)
 
 
 csvfile_path = "data/training_set_VU_DM.csv"
