68dea3ae18910e38e51335bc1cf10f647667ad5f
lclass <88320133+lclass@users.noreply.github.com>
2022-05-08 22:04:42 +0100

    basic eda, correlations, etc.

--- exploratory_data_analysis.py
+++ exploratory_data_analysis.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 
 class ExploratoryDataAnalysis:
@@ -6,82 +8,56 @@ class ExploratoryDataAnalysis:
     def __init__(self, filepath):
         self.df = pd.read_csv(filepath)
 
-        #self.reformat_prop_desirability1()
-        self.get_correlations()
-        # print(self.df['prop_location_score'].head(30).to_list())
+        self.print_EDA()
 
     def check_null_columns(self):
         """
         function that checks if a column is just composed of null values
         :return: returns a list with each col name and a boolean
         """
-        col_null_list = []
+        total = self.df.isnull().sum().sort_values(ascending=False)
+        percent = (self.df.isnull().sum() / self.df['srch_id'].count()).sort_values(ascending=False)
+        missing_data = pd.concat([total, percent], axis=1, keys=['Total', 'Percent'])
+        print(missing_data.head(len(self.df.columns.tolist())).to_string())
 
-        for col in self.df.columns:
-            bool_value = self.df[col].isnull().values.all()
-            col_null_list.append((col, bool_value))
+    def visualise_and_save_correlations(self):
+        half_df = len(self.df.columns.to_list()) // 2
+        split_df = [self.df.iloc[:, : half_df], self.df.iloc[:, half_df:]]
 
-        return col_null_list
+        for i in enumerate(split_df):
+            fig, ax = plt.subplots()
+            fig.set_size_inches(15, 10)
+            sns.heatmap(round(i[1].corr(), 2), cmap='coolwarm', ax=ax, annot=True, linewidths=2)
+            fig.savefig(f"figures/correlations{i[0]}.png")
+            fig.show()
 
-    def get_correlations(self):
+    def print_EDA(self):
+        print("""EXPLORATORY DATA ANALYSIS
+        
+        --------------------------------------------------------------------------------------------------------
+        All the features of the dataset 
+        --------------------------------------------------------------------------------------------------------
+        
+        """)
+        print(self.df.info())
 
-        new_df = self.df.copy()
-        del new_df["srch_id"]
-        del new_df["date_time"]
-        del new_df["random_bool"]
+        print("""
 
-        all_columns = list(new_df)
+        --------------------------------------------------------------------------------------------------------
+        Percentage of all NaN values for each column        
+        --------------------------------------------------------------------------------------------------------
 
-        for col in all_columns:
-            new_df[col].fillna(0, inplace=True)
-            correlation = round(new_df[col].corr(new_df['click_bool']), 2)
+        """)
+        self.check_null_columns()
 
-            print(str(col), 'Correlation:', correlation)
+        print("""
 
-    def reformat_prop_desirability1(self):
-        """
-        normalises and takes the mean of the two property desirbality scores and prints the correlation to click
-        :return: print statements and an updated df with mean values of normalised property desirability
-        """
+        --------------------------------------------------------------------------------------------------------
+        Heatmap of all correlations —–> check figures/correlations.png
+        --------------------------------------------------------------------------------------------------------
 
-        col_names = ['prop_location_score1', 'prop_location_score2']
-
-        for col in col_names:
-            minimum = self.df[col].min()
-            maximum = self.df[col].max()
-
-            self.df[col] = (round((self.df[col] - minimum) / (maximum - minimum), 4))
-
-        self.df['prop_location_score'] = round(self.df['prop_location_score1'] + self.df['prop_location_score2'], 4)
-        self.print_reformat_prop_desirability()
-
-        self.df.drop(columns=['prop_location_score1', 'prop_location_score2'])
-
-    def reformat_prop_desirability2(self):
-
-        self.df['prop_location_score'] = self.df['prop_location_score1'] + self.df['prop_location_score2']
-
-        minimum = self.df['prop_location_score'].min()
-        maximum = self.df['prop_location_score'].max()
-
-        self.df['prop_location_score'] = (round((self.df['prop_location_score'] - minimum) / (maximum - minimum), 4))
-
-        print(self.df["prop_location_score"].head(20))
-        print(f'Correlation: Click -- Prop Score1 ——> '
-              f'{round(self.df["prop_location_score"].corr(self.df["click_bool"]), 2)}')
-
-        self.df.drop(columns=['prop_location_score1', 'prop_location_score2'])
-
-    def print_reformat_prop_desirability(self):
-
-        print(f'Correlation: Click -- Prop Score1 ——> '
-              f'{round(self.df["click_bool"].corr(self.df["prop_location_score1"]), 2)}')
-        print(f'------------------------------------------------------------------------------------------------------')
-        print(f'Correlation: Click -- Prop Score2 ——> '
-              f'{round(self.df["click_bool"].corr(self.df["prop_location_score2"]), 2)}')
-        print(f'------------------------------------------------------------------------------------------------------')
-        print(f'Correlation: Click -- Prop Score Mean ——> '
-              f'{round(self.df["click_bool"].corr(self.df["prop_location_score"]), 2)}')
+        """)
+        self.visualise_and_save_correlations()
 
 
 train_set_filepath = "data/shortened_data_5000.csv"
