a33be45248a87b5c62e924f1334efc4e81023a14
lclass <88320133+lclass@users.noreply.github.com>
2022-05-11 15:02:11 +0200

    plotting function added

--- exploratory_data_analysis.py
+++ exploratory_data_analysis.py
@@ -2,12 +2,17 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+import warnings
+
+warnings.simplefilter(action='ignore', category=FutureWarning)
+
 
 class ExploratoryDataAnalysis:
 
     def __init__(self, filepath):
         self.df = pd.read_csv(filepath)
 
+        # self.save_search_affinity_score_dataframe()
         self.print_EDA()
 
     def check_null_columns(self):
@@ -30,6 +35,10 @@ class ExploratoryDataAnalysis:
             sns.heatmap(round(i[1].corr(), 2), cmap='coolwarm', ax=ax, annot=True, linewidths=2)
             fig.savefig(f"figures/correlations{i[0]}.png")
             fig.show()
+
+    def save_search_affinity_score_dataframe(self):
+        search_affinity_score_df = self.df[self.df['srch_query_affinity_score'].notna()]
+        print(search_affinity_score_df.head(100).to_string())
 
     def print_EDA(self):
         print("""EXPLORATORY DATA ANALYSIS
@@ -59,6 +68,32 @@ class ExploratoryDataAnalysis:
         """)
         self.visualise_and_save_correlations()
 
+        print("""
+
+        --------------------------------------------------------------------------------------------------------
+        Plots for columns —–> check figures/columns.png
+        --------------------------------------------------------------------------------------------------------
+
+        """)
+        #self.plotting()
+
+    def plotting(self):
+        """
+        This function plots a chart for every column. Some columns shouldnt be plotted though. It takes a long time. So
+        instead, you might want to uncomment the code and add the columns which you want to plot to that list and
+        update the for loop
+        :return: plots saved in /figures directory
+        """
+        # wanted_cols = [""]
+
+        for col in reversed(self.df.columns):
+            fig, ax = plt.subplots()
+            fig.set_size_inches(13, 8)
+            sns.countplot(col, data=self.df, order=self.df[col].unique().sort(), ax=ax)
+            fig.savefig(f"figures/{col}.png")
+            # fig.show
+
 
 train_set_filepath = "data/shortened_data_5000.csv"
 to_preprocess = ExploratoryDataAnalysis(train_set_filepath)
+
