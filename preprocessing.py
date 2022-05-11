50df376ece72920eabab1b0f498902a3aa53a58d
lclass <88320133+lclass@users.noreply.github.com>
2022-05-12 01:39:09 +0200

    price not figured out yet

--- preprocessing.py
+++ preprocessing.py
@@ -24,11 +24,11 @@ class PreProcess:
 
     def price_usd(self):
 
-        visitor_location_df = self.df[self.df["prop_country_id"].notna()]
-        location_5 = visitor_location_df[visitor_location_df['prop_country_id'] == 219]
+        visitor_location_df = self.df[self.df["site_id"].notna()]
+        location_5 = visitor_location_df[visitor_location_df['site_id'] == 5]
 
         #  print(visitor_location_df['visitor_location_country_id'].unique())
-        #  print(visitor_location_df['prop_country_id'].value_counts().to_string())
+        #  print(visitor_location_df['site_id'].value_counts().to_string())
         #  print(property_country_df.head(200).to_string())
 
         booked_location_5 = location_5[location_5['gross_bookings_usd'].notna()]
@@ -38,21 +38,23 @@ class PreProcess:
         #  print(booked_location_5['visitor_location_country_id'].value_counts().to_string())
 
         per_night_df = booked_location_5[booked_location_5['srch_length_of_stay'] == 1]
+        per_night_df = booked_location_5[booked_location_5['promotion_flag'] == 0]
 
         print("""
         PRICE PER NIGHT, TOTAL PRICE, THE PERCENTAGE OF DIFFERENCE BETWEEEN THEM"
         """)
 
         for row in range(len(per_night_df['price_usd'])):
-
             price = per_night_df['price_usd'].loc[per_night_df.index[row]]
             stay = per_night_df['srch_length_of_stay'].loc[per_night_df.index[row]]
             total_booking = per_night_df['gross_bookings_usd'].loc[per_night_df.index[row]]
-            percent = (total_booking + price*stay) // 2
+            diff = total_booking - price
+            percent = round((diff / total_booking) * 100, 2)
 
             print(price * stay, "== price per night, ", total_booking, "== total booking", "|| percent difference = ",
                   percent)
             print("---------------------------------------------------------------------------------------------------")
+
 
 csv_file = "data/shortened_data_5000.csv"
 preprocessed_csv = PreProcess(csv_file)
