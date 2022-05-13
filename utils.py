bbb824cba60ffb6a8557f30f1a0d2830a60dadca
lclass <88320133+lclass@users.noreply.github.com>
2022-05-14 00:05:59 +0200

    useful functions

--- utils.py
+++ utils.py
@@ -30,5 +30,22 @@ class CuttingData:
         self.shortened_df.to_csv(r'data/shortened_data_5000.csv', index=False, header=True)
 
 
-csvfile_path = "../original_data/training_set_VU_DM.csv"
-to_cut = CuttingData(csvfile_path)
+class CutCsv:
+
+    def __init__(self, filepath):
+        self.df = pd.read_csv(filepath)
+        self.cutting()
+
+    def cutting(self):
+        del self.df['position']
+        del self.df['click_bool']
+        del self.df['booking_bool']
+        del self.df['gross_bookings_usd']
+        self.df.to_csv(r'data/shortened_test_data_5000.csv', index=False, header=True)
+
+
+# train_example_file = "data/shortened_data_5000.csv"
+# CutCsv(train_example_file)
+#
+# csvfile_path = "../original_data/training_set_VU_DM.csv"
+# to_cut = CuttingData(csvfile_path)
