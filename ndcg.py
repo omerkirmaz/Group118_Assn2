c8f8c9801f5b035e9e3a18d4573144f06c73a2fc
lclass <88320133+lclass@users.noreply.github.com>
2022-05-24 17:49:45 +0200

    updated function

--- ndcg.py
+++ ndcg.py
@@ -12,22 +12,25 @@ def ndcg_score(true_filepath, pred_filepath):
     true_df = pd.read_csv(true_filepath)
     pred_df = pd.read_csv(pred_filepath)
 
-    all_srch_ids = set(true_df['prop_id'].values)
-    # OR:
-    # all_srch_ids = true_df['prop-id'].uniques()
+    all_srch_ids = set(true_df['srch_id'].to_list())
+
     ndcgs = []
 
     for srch_id in all_srch_ids:
-        gold_srch = true_df[true_df['srch_id'] == srch_id].to_list()
-        pred_srch = pred_df[pred_df['srch_id'] == srch_id].to_list()
 
-        true_scores = np.asarray([gold_srch['prop_id'].to_list()])
-        pred_scores = np.asarray([pred_srch['prop_id'].to_list()])
+        gold_srch = true_df[true_df['srch_id'] == srch_id]
+        gold_list = gold_srch['prop_id'].to_list()
 
-        dcg = dcg_score(true_scores, pred_scores)
-        idcg = dcg_score(true_scores, true_scores)
+        pred_srch = pred_df[pred_df['srch_id'] == srch_id]
+        pred_list = pred_srch['prop_id'].to_list()
+
+        true_scores = np.asarray([gold_list])
+        pred_scores = np.asarray([pred_list])
+
+        dcg = dcg_score(true_scores, pred_scores, k=5)
+        idcg = dcg_score(true_scores, true_scores, k=5)
 
         ndcg = dcg / idcg
-        ndcgs = ndcgs.append(ndcg)
+        ndcgs.append(ndcg)
 
     return statistics.mean(ndcgs)
